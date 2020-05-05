9c164e7715d793692b05b627a72c8d181f37c0f8
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-05 08:04:46 -0700

    new function

--- ssg/site.py
+++ ssg/site.py
@@ -6,3 +6,6 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
  
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        
