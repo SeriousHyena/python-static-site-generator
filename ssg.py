6e88feaa59b9aba4d66875407db19561a20aa9f3
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-05 10:30:13 -0700

    config options

--- ssg.py
+++ ssg.py
@@ -1,5 +1,8 @@
 '''Here we will set up the CLI.'''
 
-import typer # I will need to pip install this module
+import typer # I will need to pip install this module (https://pypi.org/project/typer/)
 from ssg.site import Site
 
+def main(source='content', dest='dist'):
+    config = {'source': source, 'dest': dest}
+    
