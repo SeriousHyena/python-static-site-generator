fe753a10325408ce51f29010b61e6e691db2a115
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-05 10:02:29 -0700

    create directory

--- ssg/site.py
+++ ssg/site.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 
 class Site:
+    
 
     def __init__(self, source, dest):
+        '''Find the root directory.'''
         self.source = Path(source)
         self.dest = Path(dest)
  
     def create_dir(self, path):
+        '''See if a directory exists before we create it.'''
         directory = self.dest / path.relative_to(self.source)
         Path.mkdir(directory, parents=True, exist_ok=True)
      
+    def build(self):
+        '''Now we can create then directory.  Path.rglob(pattern) to find the match we want'''
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
