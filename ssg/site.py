ad9583c67fb6fff126d4a945640e37101fca6689
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-05 07:56:36 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -3,6 +3,6 @@ from pathlib import Path
 class Site:
 
     def __init__(self, source, dest):
-        self.source = source(Path)
-        self.dest = dest(Path)
+        self.source = Path(source)
+        self.dest = Path(dest)
  
