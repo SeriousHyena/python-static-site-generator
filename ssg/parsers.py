9c44296ca4307ba0787ec73b515378b63fb37447
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-06 07:23:12 -0700

    parser read method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -14,3 +14,7 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
+    def read(self, path):
+        with open(path) as file: 
+            return read(path) from file 
+
