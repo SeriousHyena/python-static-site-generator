1d5e64529af0e8e328651c82b367f519648e0aa0
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-07 12:55:17 -0700

    Load class method

--- ssg/content.py
+++ ssg/content.py
@@ -5,3 +5,12 @@ from collections.abc import Mapping
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+
+    def load(self, cls, string):
+        '''according to pep 8 Always use self for the first argument to instance methods and 
+        Always use cls for the first argument to class methods.
+        https://www.python.org/dev/peps/pep-0008/#function-and-method-arguments'''
+
+        _, fm, content = cls.__regex.split(string, 2) # here we use cls per pep 8
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
