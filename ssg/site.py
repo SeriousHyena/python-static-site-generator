7245a0502d548ed4fecf10fea7aea5da12b520b4
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-07 11:36:35 -0700

    Run the parser

--- ssg/site.py
+++ ssg/site.py
@@ -27,6 +27,12 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
 
+            # test if path is a file
+            elif:
+                path.is_file()
+                Site.run_parser(path)
+
+
     def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension): # parser is an extension of Parser class so dot.chain the method
