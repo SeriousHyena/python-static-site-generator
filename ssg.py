3c6014be9c9e4d2f7759f14bb007f9db7f03c2c0
SeriousHyena <50051489+SeriousHyena@users.noreply.github.com>
2020-05-07 10:47:03 -0700

    parser configuration

--- ssg.py
+++ ssg.py
@@ -1,10 +1,12 @@
+import ssg.parsers
+
 '''Here we will set up the CLI.'''
 
 import typer # I will need to pip install this module (https://pypi.org/project/typer/)
 from ssg.site import Site
 
 def main(source='content', dest='dist'):
-    config = {'source': source, 'dest': dest}
+    config = {'source': source, 'dest': dest, 'parsers': ssg.parsers.ResourceParser()}
     Site(**config).build()
 
 typer.run(main)
